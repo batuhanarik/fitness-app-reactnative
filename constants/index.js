a8ec27c2de8c2becbe16ec8b2ff813facf55d2bc
Batuhan Arık <batuhanarik123@gmail.com>
2024-03-25 17:09:05 +0300

    carousel library installed

---
+++ constants/index.js
@@ -0,0 +1,7 @@
+export const sliderImages = [
+    require('../assets/images/slider1.png'),
+    require('../assets/images/slider2.png'),
+    require('../assets/images/slider3.png'),
+    require('../assets/images/slider4.png'),
+    require('../assets/images/slider5.png'),
+]
